fab9ea6beed30a76fe3db8209e9ab86d0df41c0e
aiglass <aiglass@oumatombp.airdream>
2016-05-19 12:04:39 +0800

    Initial Commit

---
+++ aiglassftp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  aiglassftp
+//
+//  Created by aiglass on 16/5/19.
+//  Copyright © 2016年 aiglass. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
